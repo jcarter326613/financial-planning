0fde775415d96824c796a9a2191437b44c8287b9
Jason Carter <jcarter@naturalhues.com>
2022-03-20 18:54:41 -0400

    Cleanup debug tracing

--- lambdas/src/main/controllers/stockHistoryManagement.ts
+++ lambdas/src/main/controllers/stockHistoryManagement.ts
@@ -3,18 +3,12 @@ import { Database } from "../database"
 import { LambdaRequest } from "../lambdaRequest"
 import { startCall } from "../lambdaShell"
 
-export const patch_addStockToTrack = async (...args: any[]) => startCall(args, instance.addStockToTrack)
-
-interface Response
-{
-    hello: string
-}
+export const patch_addStockToTrack = async (request: any) => startCall(request, instance.addStockToTrack)
 
 export class StockHistoryManagement
 {
     public async addStockToTrack(request: LambdaRequest): Promise<{"success": boolean}>
     {
-        console.log("addStockToTrack t1")
         // Validate the input
         let symbol: string | null = null
         if (request.queryStringParameters != null)
@@ -23,16 +17,13 @@ export class StockHistoryManagement
         }
         if (symbol == null || symbol.length == 0) throw new ArgumentsInvalidException("Missing query parameters, 'symbol'")
 
-        console.log("addStockToTrack t2")
         // Upsert the symbol into the database
         let collection = Database.instance.getStockHistoryConfigCollection()
-        console.log("addStockToTrack t3")
 
         const query = { name: symbol };
         const update = { $set: { name: symbol } };
         const options = { upsert: true };
         collection.updateOne(query, update, options);
-        console.log("addStockToTrack t4")
 
         return {"success": true}
     }
--- lambdas/src/main/lambdaShell.ts
+++ lambdas/src/main/lambdaShell.ts
@@ -2,39 +2,11 @@ import { LambdaRequest } from "./lambdaRequest"
 import { Database } from "./database"
 import { Secrets } from "./secrets"
 
-export const startCall = async (args: any[], lambda: ((request: LambdaRequest) => any)) =>
+export const startCall = async (request: any, lambda: ((request: LambdaRequest) => any)): Promise<any> =>
 {
-    console.log("startCall t1")
-    if (args.length != 3)
-    {
-        throw Error(`Invalid start to lambda function.  ${args.length} parameters`)
-    }
-    console.log("startCall t2")
-
-    return internalStartCall(args[0], args[1], args[2], lambda)
-}
-
-const internalStartCall = async (
-    request: LambdaRequest, 
-    _: any, 
-    callback: ((_1: any,_2: any) => void),
-    lambda: ((request: LambdaRequest) => any)
-    ): Promise<any> => {
-
-    console.log("internalStartCall t1")
     await Secrets.instance.initialize()
     await Database.instance.initialize()
-    console.log("internalStartCall t2")
     let result = await lambda(request)
-    console.log("internalStartCall t3")
-    /*callback(null, {
-        "statusCode": 200,
-        "headers": {
-          "Content-Type": "application/json"
-        },
-        "body": JSON.stringify(result)
-    })*/
-    console.log("internalStartCall t4")
     return {
         "statusCode": 200,
         "headers": {
